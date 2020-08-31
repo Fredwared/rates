4348ade3e56fac00710261d13d04aa32cdded3e5
Golib Khalimov <Fredwared@gmail.com>
2020-09-01 01:05:57 +0500

    Initial commit | Docker init | Swagger init

---
+++ app/Http/Controllers/Api/RatesController.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @SWG\Swagger(
+ *     basePath="/api/",
+ *     schemes={"http", "https"},
+ *     host=L5_SWAGGER_CONST_HOST,
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Rates API",
+ *         description="Rates API description",
+ *         @SWG\Contact(
+ *             email="fredwared@gmail.com"
+ *         ),
+ *     )
+ * )
+ */
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RatesController extends Controller
+{
+    //
+}
